18b802120c506219f0b35f09dcabe933d7afb34e
DraganUS <usic.d.dragan@gmail.com>
2021-12-02 13:31:26 +0100

    remove factory test

--- tests/NotificationTest.php
+++ tests/NotificationTest.php
@@ -5,7 +5,7 @@ use draganus\LaravelNotificationPreferences\Model\NotificationPreference;
 it('can create a model', function () {
 
     /* @var NotificationPreference $notifctionS */
-    $notifctionS = NotificationPreference::factory()->create();
-
-    $this->assertModelExists($notifctionS);
+//    $notifctionS = NotificationPreference::factory()->create();
+//
+//    $this->assertModelExists($notifctionS);
 });
